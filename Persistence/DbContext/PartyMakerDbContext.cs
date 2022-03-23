03e4e5d8b20f5e88be9979a4321c99cca540b276
pemix09 <pemix09@onet.pl>
2022-03-23 14:28:53 +0100

    Added reference to other projects, created Data Base Context

--- Persistence/DbContext/PartyMakerDbContext.cs
+++ Persistence/DbContext/PartyMakerDbContext.cs
@@ -1,12 +1,31 @@
+using Core.Models;
+
 namespace Persistence.DbContext
 {
     using Microsoft.AspNetCore.Identity;
     using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
     using Microsoft.EntityFrameworkCore;
 
-    public class PartyMakerDbContext : IdentityDbContext<IdentityUser,IdentityRole,string>
+    public class PartyMakerDbContext : IdentityDbContext<AppUser,IdentityRole,string>
     {
-        
+        public DbSet<Ban> Bans { get; set; }
+        public DbSet<EntrancePass> EntrancePasses { get; set; }
+        public DbSet<Event> Events { get; set; }
+        public DbSet<Message> Messages { get; set; }
+        public DbSet<MusicGenre> MusicGenres { get; set; }
+        public DbSet<Notification> Notifications { get; set; }
+        public DbSet<Report> Reports { get; set; }
+
+        public PartyMakerDbContext(DbContextOptions<PartyMakerDbContext> options) : base(options)
+        {
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(PartyMakerDbContext).Assembly);
+        }
 
     }
 }
