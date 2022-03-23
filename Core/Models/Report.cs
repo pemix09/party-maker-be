606ad68ac1346d4509231fe7d1a99201c88abd8b
pemix09 <pemix09@onet.pl>
2022-03-23 19:14:12 +0100

    Added Data Annotations to Report table

--- Core/Models/Report.cs
+++ Core/Models/Report.cs
@@ -1,11 +1,38 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Core.Models;
 
 public class Report
 {
-    public AppUser Reporter { get; set; }
-    public AppUser Reported { get; set; }
-    public DateTime Date { get; set; }
-    public string Reason { get; set; }
+    [Required]
+    [Key]
+    public uint Id { get; private set; }
+    
+    [Required]
+    public int ReporterId { get; private set; }
+    
+    [Required]
+    public int ReportedId { get; private set; }
+    
+    [Required]
+    public DateTime Date { get; private set; }
+    
+    [Required]
+    public string Reason { get; private set; }
+
+    public void SetReason(string newReason) => this.Reason = newReason;
+    private Report(){}
+    private Report(int reporterId, int reportedId, string reason)
+    {
+        this.ReporterId = reporterId;
+        this.ReportedId = reportedId;
+        this.Reason = reason;
+        this.Date = DateTime.Now;
+    }
+
+    public static Report Create(int reporterId, int reportedId, string reason)
+    {
+        return new Report(reporterId, reportedId, reason);
+    }
 }
