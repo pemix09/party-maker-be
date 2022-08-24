d15f40c1b2a060a3d0114fbc925c9962debdb8fa
pemix09 <pemix09@onet.pl>
2022-08-24 17:50:36 +0200

    small change to create migration script

--- CreateMigrationAndUpdateDB.ps1
+++ CreateMigrationAndUpdateDB.ps1
@@ -1,8 +1,58 @@
-#not tested
-#migration name:
-$migrationName = "Fix for ban model"
+Add-Type -AssemblyName System.Windows.Forms
+Add-Type -AssemblyName System.Drawing
 
-#commands to add migration and update database
-dotnet ef migrations add --startup-project .\API\API.csproj  --project .\Persistence\Persistence.csproj  $migrationName
-dotnet ef database update --startup-project .\API\API.csproj  --project .\Persistence\Persistence.csproj
+$form = New-Object System.Windows.Forms.Form
+$form.Text = 'Data Entry Form'
+$form.Size = New-Object System.Drawing.Size(300,200)
+$form.StartPosition = 'CenterScreen'
+
+$okButton = New-Object System.Windows.Forms.Button
+$okButton.Location = New-Object System.Drawing.Point(75,120)
+$okButton.Size = New-Object System.Drawing.Size(75,23)
+$okButton.Text = 'OK'
+$okButton.DialogResult = [System.Windows.Forms.DialogResult]::OK
+$form.AcceptButton = $okButton
+$form.Controls.Add($okButton)
+
+$cancelButton = New-Object System.Windows.Forms.Button
+$cancelButton.Location = New-Object System.Drawing.Point(150,120)
+$cancelButton.Size = New-Object System.Drawing.Size(75,23)
+$cancelButton.Text = 'Cancel'
+$cancelButton.DialogResult = [System.Windows.Forms.DialogResult]::Cancel
+$form.CancelButton = $cancelButton
+$form.Controls.Add($cancelButton)
+
+$label = New-Object System.Windows.Forms.Label
+$label.Location = New-Object System.Drawing.Point(10,20)
+$label.Size = New-Object System.Drawing.Size(280,20)
+$label.Text = 'Input commit message:'
+$form.Controls.Add($label)
+
+$textBox = New-Object System.Windows.Forms.TextBox
+$textBox.Location = New-Object System.Drawing.Point(10,40)
+$textBox.Size = New-Object System.Drawing.Size(260,20)
+$form.Controls.Add($textBox)
+
+$form.Topmost = $true
+
+$form.Add_Shown({$textBox.Select()})
+$result = $form.ShowDialog()
+
+#commit message:
+$CommitMessage = ""
+
+#if ok was clicked
+if ($result -eq [System.Windows.Forms.DialogResult]::OK)
+{
+    #migration name:
+    $migrationName = $textBox.Text
+
+    #commands to add migration and update database
+    dotnet ef migrations add --startup-project .\API\API.csproj  --project .\Persistence\Persistence.csproj  $migrationName
+    dotnet ef database update --startup-project .\API\API.csproj  --project .\Persistence\Persistence.csproj
+}
+
+
+
+
 
