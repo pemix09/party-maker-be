602aeb5b5ede5e442020fc4c273c83ba245e5e8b
pemix09 <pemix09@onet.pl>
2022-08-26 16:10:25 +0200

    Added [HttpPost] to Login endpoint, so swagger works again

--- API/Controllers/UserController.cs
+++ API/Controllers/UserController.cs
@@ -4,6 +4,8 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
+    [Route("[controller]/[action]")]
+    [ApiController]
     public class UserController : BaseCRUDController
     {
         public UserController(IMediator _mediator) : base(_mediator) { }
@@ -14,6 +16,8 @@ namespace API.Controllers
             await mediator.Send(command);
             return Ok();
         }
+
+        [HttpPost]
         public async Task<IActionResult> Login(LoginUserCommand command)
         {
             await mediator.Send(command);
