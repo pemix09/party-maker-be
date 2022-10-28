b987a72b3654e113c4df52cef001e2517f037ed6
pemix09 <pemix09@onet.pl>
2022-10-28 09:57:17 +0200

    MessageHub should work now

--- Infrastructure/Hubs/MessageHub.cs
+++ Infrastructure/Hubs/MessageHub.cs
@@ -1,21 +1,11 @@
 using Core.Models;
+using Microsoft.AspNetCore.SignalR;
 
 namespace Infrastructure.Hubs;
-using Microsoft.AspNetCore.SignalR;
 
 public class MessageHub : Hub<IMessageHub>
 {
     public MessageHub(){}
-
-    public async Task NewMessage(string fromUser, string content)
-    {
-        var msg = Message.Create(fromUser, fromUser, 0, content);
-        Streaming(msg);
-    }
-
-    public async IAsyncEnumerable<Message> Streaming(Message _msg)
-    {
-        yield return _msg;
-    }
+    
 
 }
